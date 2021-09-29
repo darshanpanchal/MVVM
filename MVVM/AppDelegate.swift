7d896b3f58993c74744718dbcc39e1c170f72f60
darshan <darshanpanchal619@gmail.com>
2021-09-30 00:22:42 +0530

    Update storyboard

--- MVVM/AppDelegate.swift
+++ MVVM/AppDelegate.swift
@@ -20,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
