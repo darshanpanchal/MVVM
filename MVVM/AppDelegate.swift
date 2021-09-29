b7f9836f08ee008c73d2c574cc5e3c9ca094eecf
darshan <darshanpanchal619@gmail.com>
2021-09-30 00:36:11 +0530

    developer brach commit

--- MVVM/AppDelegate.swift
+++ MVVM/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
-
+    //Developer branch changes
     
     // MARK: UISceneSession Lifecycle
 
